333fdedbef4f7c8de34aef2bb524ce9bf5f65827
Braden Niksich <abniksi@ilstu.edu>
2020-06-06 17:50:32 -0500

    Implementing quick sort

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Styles/NavigationBar.css'
 import createArray from '../HelperMethods/ArrayCreator'
-import bubbleSort from '../HelperMethods/Algorithims'
+let Algorithims = require('../HelperMethods/Algorithims')
 
 function NavigationBar(props) {
     function getNewArray(){
@@ -9,7 +9,11 @@ function NavigationBar(props) {
     }
 
     function doBubbleSort(){
-        props.setRandomNumberArray(bubbleSort(props.randomNumberArray))
+        props.setRandomNumberArray(Algorithims.bubbleSort(props.randomNumberArray))
+    }
+
+    function doQuickSort(){
+        props.setRandomNumberArray(Algorithims.quickSort(props.randomNumberArray))
     }
 
     return (
@@ -18,7 +22,7 @@ function NavigationBar(props) {
                 <div className = 'innerNavBar'>
                     <button onClick={getNewArray}>Generate New Array</button>
                     <button>Merge Sort</button>
-                    <button>Quick Sort</button>
+                    <button onClick={doQuickSort}>Quick Sort</button>
                     <button>Heap Sort</button>
                     <button onClick={doBubbleSort}>Bubble Sort</button>
                 </div>
--- src/HelperMethods/Algorithims.js
+++ src/HelperMethods/Algorithims.js
@@ -15,4 +15,29 @@ const bubbleSort = (randomNumberArray) => {
     return bubbleSortArray;
 }
 
-export default bubbleSort;
+const quickSort = (quickSortArray) => {
+    if(quickSortArray.length === 1){
+        return quickSortArray;
+    }
+
+    const pivot = quickSortArray[quickSortArray.length - 1];
+    const leftArray = [];
+    const rightArray = [];
+    for(let i = 0; i < quickSortArray.length - 1; i++){
+        if(quickSortArray[i] < pivot){
+            leftArray.push(quickSortArray[i]);
+        }else{
+            rightArray.push(quickSortArray[i]);
+        }
+    }
+
+    if(leftArray.length > 0 && rightArray.length > 0){
+        return [...quickSort(leftArray), pivot, ...quickSort(rightArray)]
+    }else if(leftArray.length > 0){
+        return [...quickSort(leftArray), pivot]
+    }else{
+        return [pivot, ...quickSort(rightArray)]
+    }
+} 
+
+export { bubbleSort, quickSort }; 
